b077fc82bbef811b48d64e79b374d393f2f9b949
Rod Forman <rodforman@hotmail.com>
2026-04-30 04:12:10 +0000

    Update 32 files
    - /English Translation/Firmware Update/.gitkeep
    - /English Translation/.gitkeep
    - /English Translation/readme.md
    - /reference/1.8.5/phrozen_dev/__init__.py
    - /reference/1.8.5/phrozen_dev/base.py
    - /reference/1.8.5/phrozen_dev/cmds.py
    - /reference/1.8.5/phrozen_dev/cwebsocketapis.py
    - /reference/1.8.5/phrozen_dev/dev.py
    - /reference/1.8.5/phrozen_dev/phrozen_dev.py
    - /reference/1.8.5/phrozen_dev/printer.cfg
    - /reference/1.8.5/printer_data/config/KlipperScreen.conf
    - /reference/1.8.5/printer_data/config/crowsnest.conf
    - /reference/1.8.5/printer_data/config/fluidd.cfg
    - /reference/1.8.5/printer_data/config/mainsail.cfg
    - /reference/1.8.5/printer_data/config/moonraker-obico-update.cfg
    - /reference/1.8.5/printer_data/config/moonraker-obico.cfg
    - /reference/1.8.5/printer_data/config/moonraker.conf
    - /reference/1.8.5/printer_data/config/printer.cfg
    - /reference/1.8.5/printer_data/config/printer_MCU.cfg
    - /reference/1.8.5/printer_data/config/printer_gcode_macro.cfg
    - /config/addon.cfg
    - /config/kaos_menu.cfg
    - /config/printer.cfg
    - /config/printer_gcode_macro.cfg
    - /config/kaos.cfg
    - /phrozen_dev/dev.py
    - /phrozen_dev/kaos_logging.py
    - /phrozen_dev/kaos_translations.py
    - /phrozen_dev/lang/kaos_translations_zh.py
    - /phrozen_dev/lang/kaos_translations_fr.py
    - /phrozen_dev/lang/lang__init__.py
    - /phrozen_dev/lang/kaos_translations_en.py

---
+++ phrozen_dev/dev.py
@@ -0,0 +1,6 @@
+####################################
+# KAOS language package marker
+#
+# This file intentionally stays empty. It allows Python to import language
+# tables from phrozen_dev.lang.
+####################################
